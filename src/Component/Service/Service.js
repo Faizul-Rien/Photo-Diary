25874737ef091c95db47c7f6d6604fa068c2171b
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-04-18 00:25:51 +0600

    add service to home page

---
+++ src/Component/Service/Service.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import "./Service.css";
+
+const Service = ({ service }) => {
+  const { img, name, price, description, rating } = service;
+  return (
+    <div className="col-12 col-md-6 col-lg-4">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" height="250px" src={img} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Service;
