07bc54cbbcce3aa4d8eb1262e5a4db70c954640c
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-04-18 17:43:10 +0600

    add about page details

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,9 +1,16 @@
 import React from "react";
+import profile from '../../Images/profile.jpg';
+import './About.css';
 
 const About = () => {
   return (
-    <div>
-      <h2>About me</h2>
+    <div className="about">
+      <div>
+        <img src={profile} alt="" />
+      </div>
+      <div>
+        <p>I'm Faizul Hassan Rien.Few months ago I completed my Bachelor's degree in computer science and engineering.After 6 months i want to see myself as a junior web developer of any IT or Sowfware related organization.My future goal is to became a full stack web developer. </p>
+      </div>
     </div>
   );
 };
